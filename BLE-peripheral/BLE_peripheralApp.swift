19e813cb58e8069bdbce1638e013b4663d913008
Wahyu Setiwan <whysetiawan27@gmail.com>
2024-11-22 21:16:29 +0700

    Initial Commit

---
+++ BLE-peripheral/BLE_peripheralApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLE_peripheralApp.swift
+//  BLE-peripheral
+//
+//  Created by Dika on 22/11/24.
+//
+
+import SwiftUI
+
+@main
+struct BLE_peripheralApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
